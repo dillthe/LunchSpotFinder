a391251e3cf8659bc3d59054f81eba25342ed03a
hunhee kim <kshkhh4015@gmail.com>
2024-03-31 14:42:00 +0900

    # dto 수정 완료

--- src/main/java/com/github/yumyum/auth/dto/LoginReqDto.java
+++ src/main/java/com/github/yumyum/auth/dto/LoginReqDto.java
@@ -2,10 +2,13 @@ package com.github.yumyum.auth.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginReqDto {
 
     private String loginId;
